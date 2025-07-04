0951510e604973a0c3e1b25d81b06800a4c70954
Anubhav03 <162338019+Anubhav03@users.noreply.github.com>
2025-07-04 19:47:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,5 +36,8 @@ def predict():
 
     return render_template('index.html', prediction=result)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # use PORT if defined, else fallback to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
